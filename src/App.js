f10acaee9ad614d38895f4a4f5de5e60e7e3bac5
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-09 22:15:43 +0100

    Add Switch component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 
 const Home = () => <h1> Stron startowa</h1>;
 const News = () => <h1> Aktualności</h1>;
 const Contact = () => <h1> Kontakt do nas</h1>;
+
+const ErrorPage = () => <h1>Strona nie istnieje</h1>;
 
 class App extends Component {
   render() {
@@ -38,9 +40,12 @@ class App extends Component {
             </nav>
           </header>
           <section>
-            <Route path='/' exact component={Home} />
-            <Route path='/news' component={News} />
-            <Route path='/contact' component={Contact} />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/news' component={News} />
+              <Route path='/contact' component={Contact} />
+              <Route component={ErrorPage} />
+            </Switch>
           </section>
         </div>
       </Router>
